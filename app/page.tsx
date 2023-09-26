b5730aa0885b3c01558ce855f34a5a385af85567
ico <francisco100eg@gmail.com>
2023-09-26 10:35:22 -0300

    :Feat: Started Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { Header } from '../components'
 
 export default function Home() {
   return (
-    <main></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {
+        <Header />
+      }
+    </main>
   )
 }
---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>Navegação</div>
+  )
+}
+
+export default Header
