4fe5b024de0e395d59202032528b2e216c2f2164
ico <francisco100eg@gmail.com>
2023-09-27 09:40:05 -0300

    :Feat: Header logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
+import "./Header.scss";
 function Header() {
   return (
-    <div>Navegação</div>
-  )
+    <div className="Header">
+      <div className="Header__logo-container">
+        <div className="Header__logo-img">
+          <Image
+            src="/logo_wBg.png"
+            alt="logo"
+            objectFit="contain"
+            fill
+            className="Header__logo"
+          />
+        </div>
+        <h2 className="Header__logo-title"><span className="color-blue">Task</span><span className="color-orange">M</span>aster</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
