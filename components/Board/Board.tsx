660d5c8cf7ceef155187a4a13be13d126843b095
ico <francisco100eg@gmail.com>
2023-10-04 17:43:42 -0300

    :Chore - added appwrite to dependencies

--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Board.scss'
 
-import { DragDropContext } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Board = () => {
   return (
-    <div className='Board'>
-        <div className="Board__container">
+    <DragDropContext className='Board'>
+        <Droppable droppableId='board' direction='horizontal' type='column'>
+            {(provided) => 
+                <div>
 
-        </div>
-    </div>
+                </div>}
+        </Droppable>
+    </DragDropContext>
   )
 }
 
