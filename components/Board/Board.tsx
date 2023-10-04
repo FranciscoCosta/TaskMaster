5011b4c646aa01829bc4bb9ae726a543f57688a8
ico <francisco100eg@gmail.com>
2023-10-04 17:36:29 -0300

    :Chore - added react-beautifull dnd types to dependencies

---
+++ components/Board/Board.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Board.scss'
+
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const Board = () => {
+  return (
+    <div className='Board'>
+        <div className="Board__container">
+
+        </div>
+    </div>
+  )
+}
+
+export default Board
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,9 @@
 import Header from "./Header";
 import AiResume from "./AiResume";
+import Board from "./Board";
 
 export {
     Header,
-    AiResume
+    AiResume,
+    Board,
 }
